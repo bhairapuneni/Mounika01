7ed204d7cad4448a4c7ab28308d7b89293105696
bhairapuneni <bairapunenimounika0707@gmail.com>
2025-03-18 16:39:58 +0530

    feature201 Changes by Vivek

--- Application.java
+++ Application.java
@@ -11,3 +11,20 @@ public class CompareStrings {
             System.out.println("Not Equal");
     }
 }
+#feature201 Changes by Vivek on October 2024 on Reports Module
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String style = new String("Bold");
+        String style2 = new String("Bold");
+
+        if(style.equals(style2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
+#end of feature201
+ }
+}
