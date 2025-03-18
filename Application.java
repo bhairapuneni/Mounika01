fd884b9a2983d4c1d93bb17967273fb4e9047ad7
bhairapuneni <bairapunenimounika0707@gmail.com>
2025-03-18 16:54:25 +0530

    feture202 Changes

--- Application.java
+++ Application.java
@@ -11,3 +11,20 @@ public class CompareStrings {
             System.out.println("Not Equal");
     }
 }
+   #feature202 Changes by Rahul on October 2024 on Payment Gateway module
+public class Factorial {
+
+    public static void main(String[] args) {
+        int num = 6;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
+# end of feature202
